ca8580e859d5d915a2e54d6fa4234e85c71e4451
shofeey <totskee12345@gmail.com>
2026-09-23 16:34:27 +0800

    y

---
+++ constants/colors.ts
@@ -0,0 +1,11 @@
+export const COLORS = {
+  primary: '#0065E1',
+  background: '#00A0F6',
+  card: '#092c61',
+  textPrimary: '#d4d9df',
+  textSecondary: '#ebedf0',
+  textOnPrimary: '#e6ebee',
+  surface: '#e1e6eb',
+  border: '#e0e9f3',
+  shadow: '#0D47A1',
+} as const;
